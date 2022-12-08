218c23296acedce1294de0a68cfa5ec8b89ad8ed
monika-czulak <czulak.monika@gmail.com>
2022-12-08 22:31:01 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach(item => {
+	const galleryItem = document.createElement('div')
+	const galleryLink = document.createElement('a')
+	const galleryImage = document.createElement('img')
+
+	gallery.append(galleryItem)
+	galleryItem.classList.add('gallery__item')
+
+	galleryItem.append(galleryLink)
+	galleryLink.classList.add('gallery__link')
+	galleryLink.href = item.original
+	// blokujemy domyślne działanie kliknięcia w link:
+	galleryLink.addEventListener('click', e => {
+		e.preventDefault()
+	})
+
+	galleryLink.append(galleryImage)
+	galleryImage.classList.add('gallery__image')
+	galleryImage.src = item.preview
+	galleryImage.dataset.source = item.original
+	galleryImage.alt = item.description
+})
